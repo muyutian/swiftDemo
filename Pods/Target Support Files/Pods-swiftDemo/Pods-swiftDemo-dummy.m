a4f31ecc254d47aa6e29d83ce6431f3f92310b0a
zhangzy <zhang434586301@163.com>
2020-11-22 23:37:50 +0800

    提交更新swift 5

--- Pods/Target Support Files/Pods-swiftDemo/Pods-swiftDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_swiftDemo : NSObject
-@end
-@implementation PodsDummy_Pods_swiftDemo
-@end
--- swiftDemo/ViewController.swift
+++ swiftDemo/ViewController.swift
@@ -2,8 +2,7 @@
 //  ViewController.swift
 //  swiftDemo
 //
-//  Created by zhangzy on 2018/6/22.
-//  Copyright © 2018年 zhangzy. All rights reserved.
+//  Created by zhangzy on 2020/11/22.
 //
 
 import UIKit
@@ -12,14 +11,63 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view.
+        
+        /**首先熟悉语法
+         let 定义常量
+         只能赋值一次，它的值不要求在编译时期确定，但使用前必须赋值
+         var 定义变量
+         常量 变量在初始化之前都需要初始化
+        */
+        
+        let a = 10
+        let b:Int
+        b = 20
+        
+        var c = a + b
+        c+=30
+        
+        let d = dd()
+        
+        print("hello world")
+        print("a=",a,"b=",b,"c=",c,"d=",d)
+        
+        
+        //大部分api和OC一致
+        
+        let view = UIView(frame:CGRect(x: 50, y: 100, width: self.view.frame.size.width-100, height: 200))
+        view.backgroundColor = UIColor.red
+        self.view .addSubview(view)
+        
+        
+        
+        /**
+         常见数据类型
+         值类型：
+         1、枚举enum（Optional）
+         2、结构体struct （Bool、Int、Float、Double、Character、String、Array、Dictionary、Set）
+         
+         引用类型：
+         类 class
+         */
+        
+        let array = [1,2,3,4]
+        let dic = ["age":26 , "height":180]
+        
+        print("数组：\(array)   字典：\(dic)")
+        
+        //元组
+        let tuple = (180, "zhangzy", 26)
+        print("元组 \(tuple)")
+        
+        let (height, name, age) = tuple
+        print("元组: \(height) \(name)  \(age)")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func dd() -> Int {
+        return 100
     }
-
 
 }
 
